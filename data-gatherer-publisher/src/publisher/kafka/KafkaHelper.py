28703b477d79f0c60823b9eeb830a66716dd11bd
Karan Patel <karanpatel4567@gmail.com>
2021-01-24 12:51:30 -0800

    Reducing poll timeout, setting queue.buffering.max.messages to max allowable

--- data-gatherer-publisher/src/publisher/kafka/KafkaHelper.py
+++ data-gatherer-publisher/src/publisher/kafka/KafkaHelper.py
@@ -11,10 +11,4 @@ class KafkaHelper:
         config_parser = ConfigParser()
         config_parser.read(os.path.join(CONFIGS_DIR, 'kafka.ini'))
 
-        return {
-            'bootstrap.servers': config_parser['DEFAULT']['bootstrap.servers'],
-            'sasl.mechanisms': config_parser['DEFAULT']['sasl.mechanisms'],
-            'security.protocol': config_parser['DEFAULT']['security.protocol'],
-            'sasl.username': config_parser['DEFAULT']['sasl.username'],
-            'sasl.password': config_parser['DEFAULT']['sasl.password']
-        }
+        return dict(config_parser['DEFAULT'])
