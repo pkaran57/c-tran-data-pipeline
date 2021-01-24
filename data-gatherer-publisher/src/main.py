359b01a1072b13b76079958070535647c63666ad
Karan Patel <karanpatel4567@gmail.com>
2021-01-24 12:30:19 -0800

    Updating main.py to accept data file as a param to publish events from

--- data-gatherer-publisher/src/main.py
+++ data-gatherer-publisher/src/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 
-from definitions import LOG_DIR
+from definitions import LOG_DIR, OUTPUT_DIR
 from downloader.BreadCrumbDataDownloader import BreadCrumbDataDownloader
 from publisher.BreadCrumbDataPublisher import BreadCrumbDataPublisher
 
@@ -14,7 +14,14 @@ logger = logging.getLogger('main')
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 if __name__ == '__main__':
+    data_file_path = None
+    if len(sys.argv) == 2:
+        data_file_name = sys.argv[1]
+        logger.info("Got {} as data file name that will be used to publish records to kafka".format(data_file_name))
+        data_file_path = os.path.join(OUTPUT_DIR, data_file_name)
+
     downloaded_data_file_path = BreadCrumbDataDownloader.download_daily_data()
-    if downloaded_data_file_path:
-        breadcrumb_records = BreadCrumbDataDownloader.load_downloaded_data(downloaded_data_file_path)
+
+    if downloaded_data_file_path or data_file_path:
+        breadcrumb_records = BreadCrumbDataDownloader.load_downloaded_data(downloaded_data_file_path if downloaded_data_file_path else data_file_path)
         BreadCrumbDataPublisher().publish_breadcrumb_records(breadcrumb_records)
