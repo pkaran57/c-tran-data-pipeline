1edbc25f8182eeffbf4d91478ec1c89b2c9ce902
Karan Patel <karanpatel4567@gmail.com>
2021-01-24 16:52:49 -0800

    Adding num_records_printer.py

---
+++ data-gatherer-publisher/src/num_records_printer.py
@@ -0,0 +1,14 @@
+# print num of records in all downloaded json files
+
+import os
+from os import listdir
+
+from definitions import DOWNLOADER_OUTPUT_DIR
+from downloader.BreadCrumbDataDownloader import BreadCrumbDataDownloader
+
+if __name__ == '__main__':
+    json_files = [file for file in listdir(DOWNLOADER_OUTPUT_DIR) if file.endswith('.json')]
+
+    for file in json_files:
+        records = BreadCrumbDataDownloader.load_downloaded_data(os.path.join(DOWNLOADER_OUTPUT_DIR, file))
+        print('Found {} records in {}'.format(len(records), file))
