0d65c9a36835c08138ff179eaff93dd2d3b0c08e
Karan Patel <karanpatel4567@gmail.com>
2021-01-24 16:53:43 -0800

    Updating num_records_printer.py

--- data-gatherer-publisher/src/num_records_printer.py
+++ data-gatherer-publisher/src/num_records_printer.py
@@ -9,6 +9,6 @@ from downloader.BreadCrumbDataDownloader import BreadCrumbDataDownloader
 if __name__ == '__main__':
     json_files = [file for file in listdir(DOWNLOADER_OUTPUT_DIR) if file.endswith('.json')]
 
-    for file in json_files:
+    for file in sorted(json_files):
         records = BreadCrumbDataDownloader.load_downloaded_data(os.path.join(DOWNLOADER_OUTPUT_DIR, file))
         print('Found {} records in {}'.format(len(records), file))
